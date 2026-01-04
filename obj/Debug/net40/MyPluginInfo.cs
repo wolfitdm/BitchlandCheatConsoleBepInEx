d4684a9c47689f4cc36dcb4ac8ac2602f894c00b
Luna Lolita <wolfitdm@gmail.com>
2026-01-04 03:15:04 +0100

    fix bugs

--- obj/Debug/net40/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace BitchlandCheatConsoleBepInEx
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "BitchlandCheatConsoleBepInEx";
-        public const string PLUGIN_NAME = "You have unlimited all!";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
